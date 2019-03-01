ab8c0d3f3d633eae9580361839be72eab77fc326
github username <github email address>
2019-03-01 09:48:50 -0500

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+
+attr_accessor :first_name, :last_name
+
+def initialize #(knowledge)
+    @knowledge = []
+end
+
+def learn
+
+end
+
+#  def first_name
+#    @first_name = first_name
+#  end
+
+end
